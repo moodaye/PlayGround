9bef27e1334c30a246df805d92b20ffb6a0a9f62
moodaye <rajiv@moodaye.com>
2016-11-19 10:26:59 -0500

    Completed sum recursion exercise from grokking

--- src/com/moodaye/playground/grokkingAlgorithms/recursion/Sum.java
+++ src/com/moodaye/playground/grokkingAlgorithms/recursion/Sum.java
@@ -3,8 +3,18 @@ package com.moodaye.playground.grokkingAlgorithms.recursion;
 public class Sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] numbersToAdd = {1,2,-3,4,5,1201};
+		System.out.println(sum(numbersToAdd));
 	}
-
+	
+	private static int sum(int[] numbersToAdd){
+		int sum = numbersToAdd[0];
+		if (numbersToAdd.length == 1) {
+			return sum;
+		}
+		int[] dest = new int[numbersToAdd.length - 1];
+		
+		System.arraycopy(numbersToAdd, 1, dest, 0, numbersToAdd.length - 1);
+		return sum += sum(dest);
+	}
 }
