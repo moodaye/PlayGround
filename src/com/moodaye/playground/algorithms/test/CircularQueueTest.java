2da4b46e5c3939b15e408bcc3594f4262f9862c8
moodaye <rajiv@moodaye.com>
2017-01-28 21:13:12 -0500

    Added test cases to test circular logic.  Seems to be working

--- src/com/moodaye/playground/algorithms/test/CircularQueueTest.java
+++ src/com/moodaye/playground/algorithms/test/CircularQueueTest.java
@@ -34,6 +34,37 @@ public class CircularQueueTest {
 		assertEquals(new Integer(2), queue.nextItem());
 	}
 	
+	@Test
+	public void testCircularGet(){
+		CircularQueue<Integer> queue = generateQueueOfInts(10);
+	
+		assertTrue(queue.isFull());
+	
+		assertEquals(new Integer(0), queue.nextItem());
+		assertEquals(new Integer(1), queue.nextItem());
+		assertEquals(new Integer(2), queue.nextItem());
+		assertEquals(new Integer(3), queue.nextItem());
+		queue.add(new Integer(10));
+		assertEquals(7, queue.size());
+		queue.add(new Integer(11));
+		queue.add(new Integer(12));
+		queue.add(new Integer(13));
+		queue.add(new Integer(14));
+		assertTrue(queue.isFull());
+		assertEquals(new Integer(4), queue.nextItem());
+		assertEquals(new Integer(5), queue.nextItem());
+		assertEquals(new Integer(6), queue.nextItem());
+		assertEquals(new Integer(7), queue.nextItem());
+		assertEquals(new Integer(8), queue.nextItem());
+		assertEquals(new Integer(9), queue.nextItem());
+		assertEquals(new Integer(10), queue.nextItem());
+		assertEquals(new Integer(11), queue.nextItem());
+		assertEquals(new Integer(12), queue.nextItem());
+		assertEquals(new Integer(13), queue.nextItem());
+		assertEquals(0, queue.size());
+		
+	}
+	
 	private CircularQueue<Integer> generateQueueOfInts(int size){
 		CircularQueue<Integer> queue = new CircularQueue<>(size);
 		
